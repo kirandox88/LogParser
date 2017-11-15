10b72a9ee54cb498f3cd4725cc668543d0b10811
Kiran <kirankumar_ramachandrappa@apple.com>
2017-11-15 15:29:05 -0800

    Added Validations

--- src/com/ef/Parser.java
+++ src/com/ef/Parser.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,15 +21,42 @@ public class Parser {
 		if(args.length == 4){
 				String filePath = args[0].split("=")[1].trim();
 				String startDate = args[1].split("=")[1].trim();
+
+				//Validate StartDate.
+				 SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd.HH:mm:ss");
+			        java.util.Date parsed;
+					try {
+						parsed = format.parse(startDate);
+					} catch (ParseException e) {
+						System.out.println("Invalid Date format, error occured while parsing, please enter date in yyyy-MM-dd.HH:mm:ss format");
+						return;
+					}
+				
 				String duration = args[2].split("=")[1].trim();
-				int threshold = Integer.parseInt(args[3].split("=")[1].trim());
+				
+				//Validate duration.
+				if(!duration.equals("hourly") && !duration.equals("daily")){
+					System.out.println("Please enter either hourly or daily for duration.");
+					return;
+				}
+				
+				//Validate Threshold.
+				int threshold = 0;
+				try{
+					 threshold = Integer.parseInt(args[3].split("=")[1].trim());	
+				}catch(NumberFormatException ex){
+					System.out.println("Please enter a valid number for threshold");
+					return;
+				}
+				
+				System.out.println("Loading the log file...");
 				boolean status = loadLogFile(filePath);
 				if(status){
 					System.out.println("Successfully loaded the log file. PATH : " + filePath);
 					List<ResponseObject> responseObjectList = parserDAO.queryLogs(startDate,duration,threshold);
 					String hours = (duration.equals("hourly")) ? "1 hour":"24 hour"; 
 					if(responseObjectList != null && responseObjectList.size() > 0){
-						System.out.println("Below are the List of IPs that have more than " + threshold + " requests starting from " + startDate +" for next "+ hours);
+						System.out.println("Below are the List of IPs that have more than " + threshold + " requests starting from " + startDate +" for next "+ hours + ":");
 						for(ResponseObject responseObject : responseObjectList){
 							if(responseObject!=null){
 								System.out.println(responseObject.getIpAddress() + " " + responseObject.getIpCount());
@@ -34,12 +64,15 @@ public class Parser {
 						}
 					}else{
 						System.out.println("There are no records present for this condition.");
+						return;
 					}
 				}else{
 					System.out.println("Could not read the file from specified path.");
+					return;
 				}
 		}else{
 			System.out.println("Please provide path, startDate, duration, threshold as command line arguments to the parser.");
+			return;
 		}
 	}
 
